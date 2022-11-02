7d2638cba6b09e76d39b6e37800759e30162fbb5
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2022-11-02 15:45:47 -0300

    Adicionada função para deletar quiz

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,8 +14,38 @@ async function getQuizzes() {
     }
 }
 
+async function deleteQuizzConfirmation(event) {
+    let cMessage = confirm("Você deseja realmente remover esse quiz?");
+    if (cMessage) await deleteQuizz(event);
+}
+
+async function deleteQuizz(quizz) {
+    //get element to get quizz id
+    try {
+        let options = {
+            method: "DELETE"
+        };
+        // if has key on div insert on options
+        await fetch("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/" + 10000, options);
+        await config();
+    } catch (error) {
+        alert("Erro ao remover quizz!");
+    }
+}
+
 function listQuizzes(quizzes) {
     let parent = document.getElementById("");
     for (let i = 0; i < quizzes.length; i++) {
+        //make logical to fill index page
     }
 }
+
+async function config() {
+    document.getElementById("container").innerHTML = "";
+    document.getElementById("buzzquizz-text").removeEventListener("click", deleteQuizzConfirmation, true);
+    document.getElementById("buzzquizz-text").addEventListener("click", deleteQuizzConfirmation, true);
+}
+
+window.onload = async function() {
+    await config();
+};
